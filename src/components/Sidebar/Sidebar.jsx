530109e54636a005f9afb789e920435a89d5ce1f
Mayur Hegde <mayurhegde11@gmail.com>
2024-12-28 19:21:43 +0530

    Sidebar structure

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            {/*Top section*/}
+            <div className="top">
+
+            </div>
+            {/*Bottom section*/}
+            <div className="bottom">
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
